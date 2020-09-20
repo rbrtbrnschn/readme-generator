f14f7ee2c54ea49b5775521903a86a01118f187f
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-09-20 03:03:32 +0200

    Added 80% Functionality

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SRC=$PWD
+$SRC/dependencies.sh
---
+++ syntax.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+TEMP=".syntax"
+FINAL="README.md"
+CHECK=".temp"
+
+CODE="s/CODE/\`/g"
+CBLOCK="s/CBLOCK/\n\`\`\`\n/g"
+CBLOCKLANG="s/CBLOCKLANG/\`\`\`/g"
+BREAK="s/BREAK/\<hr\/\>/g"
+BOLD="s/BOLD/\*\*/g"
+ITALIC="s/ITALIC/\*/g"
+
+REGEXES=($CODE $CBLOCK $CBLOCKLANG $BREAK $BOLD $ITALIC)
+#REGEXES=( $REGEXES )
+
+if [ -f $FILE ]; then rm $FILE; fi
+for REG in ${REGEXES[@]}
+do
+	echo $REG
+	perl -pe $REG $CHECK >> $TEMP
+	cat $TEMP > $FINAL
+	CHECK="$FINAL"
+	rm $TEMP
+done
+
+rm $TEMP
