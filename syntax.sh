357baca07e3e0abe14028523e1bbf2d16d4f61b2
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2020-09-21 21:43:42 +0200

    Bugfix

--- syntax.sh
+++ syntax.sh
@@ -4,13 +4,12 @@ FINAL="$HOME/README.md"
 CHECK="$HOME/.temp"
 
 CODE="s/CODE/\`/g"
-CBLOCK="s/CBLOCK/\n\`\`\`\n/g"
-CBLOCKLANG="s/CBLOCKLANG/\`\`\`/g"
+CBLOCK="s/CBLOCK/\`\`\`/g"
 BREAK="s/BREAK/\<hr\/\>/g"
 BOLD="s/BOLD/\*\*/g"
 ITALIC="s/ITALIC/\*/g"
 
-REGEXES=($CBLOCKLANG $CODE $CBLOCK $BREAK $BOLD $ITALIC)
+REGEXES=($CODE $CBLOCK $BREAK $BOLD $ITALIC)
 #REGEXES=( $REGEXES )
 
 #if [ -f $TEMP]; then rm $TEMP; fi
